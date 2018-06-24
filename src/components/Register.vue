900543a1243022becaaf786ce5df2312f9505a49
Ashish Wadekar <ashishawadekar@gmail.com>
2018-06-24 17:22:58 +0530

    Add Register & Login Components Blueprint

---
+++ src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <div class="login card-panel grey lighten-4 black-text center">
+                        <h3>Registration</h3>
+                        <form action="index.html">
+                            <div class="input-field">
+                                <i class="material-icons prefix">email</i>
+                                <input type="email" id="email" v-model="email">
+                                <label class="black-text" for="email">Email Address</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input type="password" id="password" v-model="password">
+                                <label class="black-text" for="password">Password</label>
+                            </div>
+                            <div class="input-field">
+                                <i class="material-icons prefix">lock</i>
+                                <input type="password" id="confirm-password" v-model="confirm_password">
+                                <label class="black-text" for="confirm-password">Confirm Password</label>
+                            </div>
+                            <button v-on:click="register" class="btn btn-large btn-extended blue white-text">Register</button>
+                            <br><br>
+                            <router-link to='login' class="black-text">Already have an account ? Click here to login</router-link>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'register',
+    data: function() {
+        return {}
+    },
+    methods: {
+        register() {
+
+        }
+    }
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Dashboard from '@/components/Dashboard'
 import NewContact from '@/components/NewContact'
 import EditContact from '@/components/EditContact'
 import ViewContact from '@/components/ViewContact'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -13,6 +15,16 @@ export default new Router({
       path: '/',
       name: 'dashboard',
       component: Dashboard
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     },
     {
       path: '/new',
