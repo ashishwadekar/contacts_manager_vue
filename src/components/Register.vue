8a7f118a2f60f16cbc346b2d9a09e017b17db32f
Ashish Wadekar <ashishawadekar@gmail.com>
2018-06-24 17:37:18 +0530

    Add registration logic [WIP]

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -5,23 +5,23 @@
                 <div class="col s12 m8 offset-m2">
                     <div class="login card-panel grey lighten-4 black-text center">
                         <h3>Registration</h3>
-                        <form action="index.html">
+                        <form @submit.prevent="register">
                             <div class="input-field">
                                 <i class="material-icons prefix">email</i>
-                                <input type="email" id="email" v-model="email">
+                                <input type="email" id="email" v-model="email" required>
                                 <label class="black-text" for="email">Email Address</label>
                             </div>
                             <div class="input-field">
                                 <i class="material-icons prefix">lock</i>
-                                <input type="password" id="password" v-model="password">
+                                <input type="password" id="password" v-model="password" required>
                                 <label class="black-text" for="password">Password</label>
                             </div>
                             <div class="input-field">
                                 <i class="material-icons prefix">lock</i>
-                                <input type="password" id="confirm-password" v-model="confirm_password">
+                                <input type="password" id="confirm-password" v-model="confirm_password" required>
                                 <label class="black-text" for="confirm-password">Confirm Password</label>
                             </div>
-                            <button v-on:click="register" class="btn btn-large btn-extended blue white-text">Register</button>
+                            <button type="submit" class="btn btn-large btn-extended blue white-text">Register</button>
                             <br><br>
                             <router-link to='login' class="black-text">Already have an account ? Click here to login</router-link>
                         </form>
@@ -33,13 +33,32 @@
 </template>
 
 <script>
+import firebase from "firebase"
+
 export default {
     name: 'register',
     data: function() {
-        return {}
+        return {
+            email: null,
+            password: null,
+            confirm_password: null
+        }
     },
     methods: {
         register() {
+            if(this.password === this.confirm_password) {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then( user => {
+                    alert('Account created for ' + this.email)
+                    this.$router.push('/')
+                },
+                err => {
+                    alert(err.message)
+                })
+            } else {
+                alert('Passwords do not match! Please check.')
+                this.password = null
+                this.confirm_password = null
+            }
 
         }
     }
